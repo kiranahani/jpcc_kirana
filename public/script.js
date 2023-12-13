0ba3ef991cfc2615a58b30da43159f8fe59d1f53
Levine Lighto <lighto.cc2@gmail.com>
2023-12-13 22:50:24 +0800

    Image with polaroid frame

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 let globalImageUrl  = '' // Variable to store the image URL globally
 let globalImageBlob
+let polaroidBlob
 
 // Function to fetch image as a blob
 function fetchImageAsBlob(imageUrl) {
@@ -9,60 +10,114 @@ function fetchImageAsBlob(imageUrl) {
 }
 
 /**
+ * Hide element when after user submit
+ */
+function startLoading() {
+
+    const processingIndicator = document.getElementById('processingIndicator');
+    const loadingIndicator  = document.getElementById('loadingIndicator');
+    const generatedImage    = document.getElementById('generatedImage');
+    const apiLimitMessage   = document.getElementById('apiLimitMessage');
+    const downloadButton    = document.getElementById('downloadButton');
+    const shareButton       = document.getElementById('shareButton');
+    const form              = document.getElementById('cardForm');
+
+    processingIndicator.style.display = 'none';
+    loadingIndicator.style.display  = 'block';
+    generatedImage.style.display    = 'none';
+    apiLimitMessage.style.display   = 'none';
+    downloadButton.style.display    = 'none';
+    shareButton.style.display       = 'none';
+
+    form.setAttribute('disabled', true)
+
+}
+
+/**
  * Convert base 64 string to image blob
  * 
  * @param {string} base64 Base 64 encoded image
  * @param {string} contentType mimetype of the image
  * @returns Blob object of the image
  */
-function base64ToBlob(base64, contentType = 'image/png') {
+async function base64ToBlob(base64, contentType = 'image/png') {
+    return new Promise(resolve => {
+        const sliceSize = 512
 
-    const sliceSize = 512
+        const byteCharacters    = atob(base64);
+        const byteArrays        = [];
 
-    const byteCharacters    = atob(base64);
-    const byteArrays        = [];
+        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
 
-    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            const slice = byteCharacters.slice(offset, offset + sliceSize)
 
-        const slice = byteCharacters.slice(offset, offset + sliceSize)
+            const byteNumbers = new Array(slice.length)
+            for (let i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i)
+            }
 
-        const byteNumbers = new Array(slice.length)
-        for (let i = 0; i < slice.length; i++) {
-            byteNumbers[i] = slice.charCodeAt(i)
+            const byteArray = new Uint8Array(byteNumbers)
+            byteArrays.push(byteArray)
+
         }
 
-        const byteArray = new Uint8Array(byteNumbers)
-        byteArrays.push(byteArray)
+        const blob = new Blob(byteArrays, {type: contentType})
+        resolve(blob)
+    })
+}
 
+/**
+ * Merge generated image with the polaroid
+ * 
+ * @param {Blob} image the image blob
+ * @returns 
+ */
+async function mergeImage(image) {
+    const processingIndicator = document.querySelector('#processingIndicator')
+    processingIndicator.style.display = 'block'
+
+    try {
+
+        const canvas    = document.querySelector('#drawingCanvas')
+        const context   = canvas.getContext('2d')
+
+        context.drawImage(await createImageBitmap(polaroidBlob), 0, 0)
+        context.drawImage(await createImageBitmap(image), 152, 45, 718, 718)
+
+        canvas.toBlob(blob => {
+            globalImageBlob = blob
+
+            persistGeneratedImage()
+        })
+        
+    } catch (error) {
+
+        console.error('Error:',error)
+        alert('An error occured while merging image')
+
+    } finally {
+        processingIndicator.style.display = 'none'
     }
-
-    const blob = new Blob(byteArrays, {type: contentType})
-    return blob
 
 }
 
 /**
  * Persist generated image into the server
- * 
- * @param {string} imageUrl the image url hosted by DALL-E
  */
-async function persistGeneratedImage(imageUrl) {
+async function persistGeneratedImage() {
 
-    if (!imageUrl) {
-        alert('Invalid image url')
-        return
-    }
+    const persistingIndicator = document.querySelector('#persistingIndicator')
+    persistingIndicator.style.display = 'block'
 
     try {
 
+        const formData = new FormData
+
+        formData.append('image', globalImageBlob)
+
         const response = await fetch('/persist-generated-image', {
             method: 'POST',
-            body: JSON.stringify({
-                imageUrl: imageUrl
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            body: formData,
         })
 
         if (!response.ok) {
@@ -70,25 +125,38 @@ async function persistGeneratedImage(imageUrl) {
         }
 
         const data = await response.json()
-        if (!data.imageUrl || 
-            !data.blob || 
-            !data.blob
-        ) {
+        if (!data.imageUrl) {
             throw new Error('Invalid response from backend')
         }
 
         globalImageUrl = data.imageUrl
-        globalImageBlob= base64ToBlob(data.blob)
 
-        const shareButton = document.getElementById('shareButton');
+        const shareButton       = document.querySelector('#shareButton')
+        const downloadButton    = document.querySelector('#downloadButton')
+        const imageDisplay      = document.querySelector('#generatedImage')
 
-        shareButton.removeAttribute('disabled')
+        if (!shareButton ||
+            !downloadButton ||
+            !imageDisplay
+        ) {
+            alert("Missing elements!")
+            return
+        }
+
+        imageDisplay.setAttribute('src', globalImageUrl)
+        
+        imageDisplay.style.display      = 'block'
+        downloadButton.style.display    = 'block'
+        shareButton.style.display       = 'block'
 
     } catch (error) {
         
         console.error('Error:', error);
         alert('An error occurred while persisting the image.');
 
+    } finally {
+        persistingIndicator.style.display = 'none';
+        
     }
 }
 
@@ -96,55 +164,49 @@ async function persistGeneratedImage(imageUrl) {
 document.getElementById('cardForm').addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const description = document.getElementById('description').value;
-    const customText = document.getElementById('customText').value;
-    const loadingIndicator = document.getElementById('loadingIndicator');
-    const generatedImage = document.getElementById('generatedImage');
-    const apiLimitMessage = document.getElementById('apiLimitMessage');
-    const downloadButton = document.getElementById('downloadButton');
-    const shareButton = document.getElementById('shareButton');
+    const description       = document.getElementById('description').value;
+    const customText        = document.getElementById('customText').value;
+    const loadingIndicator  = document.getElementById('loadingIndicator');
 
-    loadingIndicator.style.display = 'block';
-    generatedImage.style.display = 'none';
-    apiLimitMessage.style.display = 'none';
-    downloadButton.style.display = 'none';
-    shareButton.style.display = 'none';
+    startLoading()
 
     try {
-        const response = await fetch('/generate-image', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ description, customText }),
-        });
+        const [imageResponse, polaroidResponse] = await Promise.all([
+            fetch('/generate-image', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ description, customText }),
+            }),
+            fetch(`${location.href}assets/polaroid.png`)
+        ]);
 
-        if (response.status === 429) {
+        if (imageResponse.status === 429) {
             apiLimitMessage.style.display = 'block';
             return
         } 
 
-        if (!response.ok) {
-            throw new Error(`Server responded with status: ${response.status}`);
+        if (!imageResponse.ok) {
+            throw new Error(`Server responded with status: ${imageResponse.status}`);
         }
 
-        const data = await response.json();
+        const data = await imageResponse.blob();
 
-        if (!data.imageUrl) {
-            alert('No image was returned. Please try again.');
+        if (!polaroidResponse.ok) {
+            alert('Failed to fetch the polaroid')
             return
         }
+        polaroidBlob = await polaroidResponse.blob()
 
-        globalImageUrl  = data.imageUrl;
+        // To Do: Show progress
+        console.log("Image successfully generated, processing the image ...")
 
-        generatedImage.src = data.imageUrl;
-        generatedImage.style.display = 'block';
-        downloadButton.style.display = 'block';
-        shareButton.style.display = 'block';
-
-        persistGeneratedImage(data.imageUrl)
+        await mergeImage(data)
         
     } catch (error) {
+
         console.error('Error:', error);
         alert('An error occurred while generating the image.');
+
     } finally {
         loadingIndicator.style.display = 'none';
     }
